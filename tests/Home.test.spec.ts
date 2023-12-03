a7f4e824908a2edb85775ae152c995067ab46032
SumanJayaprakash <jayaprakashsuman@gmail.com>
2023-12-03 19:06:09 +0100

    updated

--- tests/Home.test.spec.ts
+++ tests/Home.test.spec.ts
@@ -21,7 +21,7 @@ test.describe("Home Feature @home", async () => {
         
     })
 
-    test("TC07:Positive: Home Page - Expected titles Home Product Contact and Signout", async ({ page }) => {
+    test("TC07:Positive: Home Page - Expected titles Home Product Contact and Signout", async ({ page },testInfo) => {
         
         //Assertions
         expect(await homepage.homelink).toContainText;
@@ -31,18 +31,10 @@ test.describe("Home Feature @home", async () => {
         expect(page.getByText('Home')).toBeTruthy()
         expect(page.getByText('Products')).toBeTruthy()
         expect(page.getByText('Contact')).toBeTruthy()
-        
+        testInfo.setTimeout(testInfo.timeout + 30000);
+                 
     });
-    test("TC08:Positive: Click on UserIcon", async ({ page }) => {
-         //Assertions
-         expect(await homepage.homelink).toContainText;
-        await expect(page.getByText('Home')).toHaveText('Home')
-        //await expect(page.locator('i').nth(3)).toBeEnabled()
-        await expect(page.locator('i').nth(3)).toBeHidden()
-       // await page.locator('i').nth(3).click();
-        //await expect(page.getByText('Home Products Contact Sign Out')).toHaveText('Home Products Contact')
-          
-    });
+
 
 
 })
